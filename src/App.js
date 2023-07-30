392cd65273cf3ad06632821b0b108480aaf63dd1
bherdyanto26 <71887357+Menrva-pixel@users.noreply.github.com>
2023-07-30 15:02:04 +0700

    service worker update
    service worker added!
    workbox

--- src/App.js
+++ src/App.js
@@ -30,5 +30,17 @@ const App = () => {
   );
 };
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('Service Worker registered: ', registration);
+    }).catch((error) => {
+      console.error('Service Worker registration failed: ', error);
+    });
+  });
+}
+
+
+
 
 export default App;
